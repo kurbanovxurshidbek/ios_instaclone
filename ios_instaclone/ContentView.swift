61dd151d1758f80eea1b9687fa86989bf2e815ed
khurshid88 <shamsun.com@gmail.com>
2021-08-12 10:00:29 +0500

    Initial Commit

---
+++ ios_instaclone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_instaclone
+//
+//  Created by KHURSHIDBEK KURBANOV on 2021/08/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
